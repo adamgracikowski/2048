476eb1a53afefa6a154f450c32f10f9f60a403c7
Adam <admgrac@gmail.com>
2023-09-07 10:58:50 +0200

    create core folder and add Tile class

---
+++ core/Tile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _2048.core
+{
+    public class Tile
+    {
+        private static readonly int multiplier = 2;
+        public static readonly int EmptyValue = 1;
+        public static readonly int MaxValue = 2048;
+
+        public int Value { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public bool WasUpdated { get; set; }
+
+        public Tile(int row, int col, int value = 1, bool wasUpdated = false)
+        {
+            Row = row;
+            Col = col;
+            Value = value;
+            WasUpdated = wasUpdated;
+        }
+        public bool IsEmpty()
+        {
+            return Value == EmptyValue;
+        }
+        public void MakeEmpty()
+        {
+            Value = EmptyValue;
+        }
+        public void IncreaseValue()
+        {
+            Value *= multiplier;
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
